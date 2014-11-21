a5fe2a6920e003f47747d69efe9bb92df9c2036d
julyMP <marispopiuliana@gmail.com>
2014-11-21 23:39:11 +0300

    First commit

---
+++ src/ro/julymp/chat/MainClass.java
@@ -0,0 +1,14 @@
+package ro.julymp.chat;
+
+/**
+@author Marius-Pop Iuliana Nov 21, 2014
+ */
+
+public class MainClass {
+
+    public static void main(String[] args) {
+	// TODO Auto-generated method stub
+
+    }
+
+}
